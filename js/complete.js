fb59ab2727452b23891efedff88df1620851c02b
mac1 <mac1@MacBook-Pro.local>
2017-08-13 23:59:00 +0900

    baseContainerの子オブジェクトにnameプロパティを設定しました。

--- js/complete.js
+++ js/complete.js
@@ -12,6 +12,7 @@ function handleComplete() {
         // 基準点を調整する
         background.regX = 0;
         background.regY = 0;
+        background.name = 'background';
         baseContainer.addChild(background);
         break;
       case "l1_cloud_under01":
@@ -22,6 +23,7 @@ function handleComplete() {
         l1CloudUnder01.x = l1CloudUnder01.subX;
         // l1CloudUnder01.y = Math.round((Math.random() * (95 - 80) + 80) * ZOOM_MAGNI);
         l1CloudUnder01.y = Math.round(80);//(80 * ZOOM_MAGNI);
+        l1CloudUnder01.name = 'l1CloudUnder01';
         baseContainer.addChild(l1CloudUnder01);
         break;
       case "l1_cloud_top01":
@@ -32,6 +34,7 @@ function handleComplete() {
         l1CloudTop01.x = l1CloudTop01.subX;
         // l1CloudTop01.y = Math.round((Math.random() * (65 - 45) + 45) * ZOOM_MAGNI);
         l1CloudTop01.y = Math.round(45);//(45 * ZOOM_MAGNI)
+        l1CloudTop01.name = 'l1CloudTop01';
         baseContainer.addChild(l1CloudTop01);
         break;
       case "l1_forest01":
@@ -41,6 +44,7 @@ function handleComplete() {
         l1Forest01.subX = Math.random() * (STAGE_W - 0) + 0;
         l1Forest01.x = l1Forest01.subX;
         l1Forest01.y = (132 - 8);// * ZOOM_MAGNI
+        l1Forest01.name = 'l1Forest01';
         baseContainer.addChild(l1Forest01);
         break;
       case "l1_forest02":
@@ -50,6 +54,7 @@ function handleComplete() {
         l1Forest02.subX = Math.random() * (STAGE_W - 0) + 0;
         l1Forest02.x = l1Forest02.subX;
         l1Forest02.y = (132 - 8);// * ZOOM_MAGNI
+        l1Forest02.name = 'l1Forest02';
         baseContainer.addChild(l1Forest02);
         break;
       case "l2_tree01":
@@ -59,6 +64,7 @@ function handleComplete() {
         l2Tree01.subX = Math.random() * (STAGE_W - 0) + 0;
         l2Tree01.x = l2Tree01.subX;
         l2Tree01.y = (133 - 24);// * ZOOM_MAGNI
+        l2Tree01.name = 'l2Tree01';
         baseContainer.addChild(l2Tree01);
         break;
       case "spritesheet_char":
@@ -92,6 +98,7 @@ function handleComplete() {
         spriteChar.subX = (STAGE_W / 2) - (CHAR_W / 2);// 画面中央 - キャラサイズ半分分ずらす
         spriteChar.x = spriteChar.subX;
         spriteChar.y = GROUND_LINE - CHAR_H;//
+        spriteChar.name = 'spriteChar';
         baseContainer.addChild(spriteChar);
         break;
       case "l4_grass01":
@@ -103,14 +110,17 @@ function handleComplete() {
         l4Grass01Obj1.subX = 0;// * ZOOM_MAGNI
         l4Grass01Obj1.x = l4Grass01Obj1.subX;
         l4Grass01Obj1.y = STAGE_H - 24;// * ZOOM_MAGNI
+        l4Grass01Obj1.name = 'l4Grass01Obj1';
         baseContainer.addChild(l4Grass01Obj1);
         l4Grass01Obj2.subX = 120;// * ZOOM_MAGNI
         l4Grass01Obj2.x = l4Grass01Obj2.subX;
         l4Grass01Obj2.y = STAGE_H - 24;// * ZOOM_MAGNI
+        l4Grass01Obj2.name = 'l4Grass01Obj2';
         baseContainer.addChild(l4Grass01Obj2);
         l4Grass01Obj3.subX = 240;// * ZOOM_MAGNI
         l4Grass01Obj3.x = l4Grass01Obj3.subX;
         l4Grass01Obj3.y = STAGE_H - 24;// * ZOOM_MAGNI
+        l4Grass01Obj3.name = 'l4Grass01Obj3';
         baseContainer.addChild(l4Grass01Obj3);
         break;
       case "se_jump":
